25539128b8bfdb3ceb2417aff77ac50f42adecf3
behike56 <living.tech.dancing.code@gmail.com>
2024-07-12 16:18:25 +0900

    updated.

--- orders/api/api.py
+++ orders/api/api.py
@@ -5,6 +5,7 @@ from starlette.responses import Response
 from starlette import status
 
 from orders.app import app
+from orders.api.schemas import CreateOrderSchema
 
 orders = {
     'id': '1234567890',
@@ -12,7 +13,7 @@ orders = {
     'created': datetime.utcnow(),
     'order': [
         {
-            'product': 'cappuccino',
+            # 'product': 'cappuccino',
             'size': 'medium',
             'quantity': 1
         }
@@ -26,7 +27,7 @@ def get_orders():
 
 
 @app.post('/orders', status_code=status.HTTP_201_CREATED)
-def create_order():
+def create_order(order_details: CreateOrderSchema):
     return orders
 
 
@@ -36,7 +37,7 @@ def get_order(order_id: UUID):
 
 
 @app.put('/orders/{order_id}')
-def update_order(order_id: UUID):
+def update_order(order_id: UUID, order_details: CreateOrderSchema):
     return orders
 
 
@@ -53,3 +54,7 @@ def cancel_order(order_id: UUID):
 @app.post('/orders/{order_id}/pay')
 def pay_order(order_id: UUID):
     return orders
+
+
+def test():
+    pass
--- orders/app.py
+++ orders/app.py
@@ -3,3 +3,5 @@ from fastapi import FastAPI
 app = FastAPI(debug=True)
 
 from orders.api import api
+
+api.test()
