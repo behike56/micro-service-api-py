e7b309792d34ffb2ed4b6f6c38c812f6df515bd4
behike56 <living.tech.dancing.code@gmail.com>
2024-07-12 20:52:36 +0900

    add.

--- orders/api/api.py
+++ orders/api/api.py
@@ -1,3 +1,5 @@
+import uuid
+
 from datetime import datetime
 from uuid import UUID
 
@@ -5,30 +7,46 @@ from starlette.responses import Response
 from starlette import status
 
 from orders.app import app
-from orders.api.schemas import CreateOrderSchema
+from orders.api.schemas import (
+    GetOrderSchema,
+    CreateOrderSchema,
+    GetOrdersSchema
+    )
 
-orders = {
-    'id': '1234567890',
-    'status': 'deliverd',
-    'created': datetime.utcnow(),
-    'order': [
-        {
-            # 'product': 'cappuccino',
-            'size': 'medium',
-            'quantity': 1
-        }
-    ]
-}
+# orders: dict = {
+#     'id': '1234567890',
+#     'status': 'deliverd',
+#     'created': datetime.utcnow(),
+#     'updated': datetime.utcnow(),
+#     'order': [
+#         {
+#             'product': 'coca coffee',
+#             'size': 'small',
+#             'quantity': 1
+#         }
+#     ]
+# }
+
+ORDERS: list = []
 
 
-@app.get('/orders')
+@app.get('/orders', response_model=GetOrdersSchema)
 def get_orders():
-    return {'orders': [orders]}
+    return {'orders': ORDERS}
 
 
-@app.post('/orders', status_code=status.HTTP_201_CREATED)
-def create_order(order_details: CreateOrderSchema):
-    return orders
+@app.post('/orders',
+          status_code=status.HTTP_201_CREATED,
+          response_model=GetOrderSchema)
+def create_order(order_details: CreateOrderSchema) -> dict:
+
+    order = order_details.dict()
+    order['id'] = uuid.uuid4()
+    order['created'] = datetime.utcnow()
+    order['status'] = 'created'
+    ORDERS.append(order)
+    
+    return order
 
 
 @app.get('/orders/{order_id}')
